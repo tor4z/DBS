1895a844ab2488e78c7e64cbe15f5ce269f2cf2c
tor4z <vwenjie@hotmail.com>
2020-06-29 10:25:45 +0800

    init gcn

--- model.py
+++ model.py
@@ -1,8 +1,40 @@
+import numpy as np
 import torch
 from torch import nn
 from torch.nn import functional as F
 
 from resnet import resnet
+
+
+def normalize_adj(A):
+    D = torch.pow(A.sum(1).float(), -0.5)
+    D = torch.diag(D)
+    adj = torch.matmul(torch.matmul(A, D).t(), D)
+    return adj
+
+
+class GraphConv(nn.Module):
+    def __init__(self, in_planes, out_planes, bias=False):
+        super(GraphConv, self).__init__()
+        self.weight = nn.Parameter(torch.Tensor(in_planes, out_planes), requires_grad=True)
+        self.adj_weight = nn.Parameter(torch.ones(10, out_planes), requires_grad=True)
+        if bias: self.bias = nn.Parameter(torch.Tensor(out_planes), requires_grad=True)
+        else: self.register_buffer('bias', None)
+        self.init_weight()
+
+    def init_weight(self):
+        stdv = 1. / np.sqrt(self.weight.size(1))
+        self.weight.data.uniform_(-stdv, stdv)
+        if self.bias is not None:
+            self.bias.data.uniform_(-stdv, stdv)
+
+    def forward(self, inp, adj):
+        support = torch.mm(inp, self.weight)
+        output = torch.mm(adj, self.adj_weight * support)
+        # output = torch.mm(adj, support)
+        if self.bias is not None:
+            output += self.bias
+        return output
 
 
 class Classifier(nn.Module):
@@ -12,42 +44,49 @@ class Classifier(nn.Module):
 
     def __init__(self, opt):
         super(Classifier, self).__init__()
+        self.enable_gcn = opt.enable_gcn
         self.backbone = resnet(opt)
         self.feat_dim = self.backbone.expansion * 512
-        anchor = torch.randn(opt.num_classes, self.feat_dim)
-        self.anchor = nn.Parameter(anchor, requires_grad=True)
+        self.ac = nn.Sequential(
+            nn.BatchNorm1d(10),
+            nn.Softmax(dim=1)
+        )
 
-    @classmethod
-    def fix_range(cls, x):
-        return (x + 1) / 2
-
-    def l2_norm(self, x):
-        return x / (x.norm(dim=1).view(-1, 1) + self.ESP)
+        self.adj = nn.Parameter(1 - torch.eye(10), requires_grad=True)
+        self.inp = nn.Parameter(torch.ones(10, 1), requires_grad=True)
+        self.gcn1 = GraphConv(1, 128)
+        self.gcn2 = GraphConv(128, self.feat_dim)
 
     def pred(self, dist):
         return dist.argmax(dim=1)
 
-    def forward(self, x, y):
-        x = self.backbone(x)
-        x = F.adaptive_avg_pool2d(x, (1, 1))
-        x = x.view(x.size(0), -1)
-
-        x = self.l2_norm(x)
-        anchor = self.l2_norm(self.anchor)
-
-        dist = torch.mm(x, anchor.t())
-        dist = self.fix_range(dist)
-
+    def criteria(self, x, y):
         pos_mask = y
         neg_mask = 1 - y
 
         pos_cnt = pos_mask.sum(dim=1) + self.ESP
         neg_cnt = neg_mask.sum(dim=1) + self.ESP
 
-        pos_loss = -1 * (pos_mask * (self.K * dist + self.B).log()).sum(dim=1) / pos_cnt
-        neg_loss = -1 * (neg_mask * (1 - self.K * dist).log()).sum(dim=1) / neg_cnt
-        loss = (pos_loss + neg_loss).mean()
-        
-        pred = self.pred(dist)
+        pos_loss = -1 * (pos_mask * (x * self.K + self.B).log()).sum(dim=1) / pos_cnt
+        neg_loss = -1 * (neg_mask * (1 - x * self.K).log()).sum(dim=1) / neg_cnt
+
+        return (pos_loss + neg_loss).mean()
+
+    def forward(self, x, y):
+        x = self.backbone(x)
+        x = F.adaptive_avg_pool2d(x, (1, 1))
+        x = x.view(x.size(0), -1)
+
+        if self.enable_gcn:
+            adj = normalize_adj(self.adj)
+            inp = self.gcn1(self.inp, adj)
+            inp = F.relu(inp)
+            inp = self.gcn2(inp, adj)
+            inp = F.relu(inp)
+            x = torch.mm(x, inp.t())
+
+        x = self.ac(x)
+        loss = self.criteria(x, y)
+        pred = self.pred(x)
 
         return loss, pred
--- train.py
+++ train.py
@@ -10,11 +10,12 @@ from trainer import Trainer
 def main(opt):
     trainer = Trainer(opt)
     train_dataset, validate_dataset, test_dataset = get_train_val_test_dataset(opt)
-    train_loader = DataLoader(train_dataset, batch_size=opt.batch_size, shuffle=opt.shuffle,\
+    train_loader = DataLoader(train_dataset, batch_size=opt.batch_size,\
                               num_workers=opt.num_workers, drop_last=opt.drop_last)
     validate_loader = DataLoader(validate_dataset, batch_size=opt.batch_size,\
                               num_workers=opt.num_workers, drop_last=opt.drop_last)
-    test_loader = DataLoader(test_dataset, batch_size=opt.batch_size, num_workers=opt.num_workers)
+    test_loader = DataLoader(test_dataset, batch_size=opt.batch_size,\
+                              num_workers=opt.num_workers)
 
     trainer.train(train_loader, validate_loader, test_loader)
 
